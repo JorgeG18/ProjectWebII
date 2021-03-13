63124e1a6ad5664773a6912f6bfeda366436763e
JorgeG18 <jorge1giron18@gmail.com>
2021-03-13 16:33:20 -0600

    agregando las view y implementando el slider

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'pagina_principal') -> name ('pagina_principal');
+
+
