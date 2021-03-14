ba54c00e706a04fdc35060d4c638ae7b7dced139
Hugo Murillo Urbina <hmurillo464@hotmail.com>
2021-03-14 14:00:04 -0600

    Cambios

--- resources/views/acercade.blade.php
+++ resources/views/acercade.blade.php
@@ -6,69 +6,70 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-lg-4" id="j">
-            <img class="img-circle" src="https://g2consultores.com.mx/wp-content/uploads/2015/04/2010112419192700001105280000193577.jpg" alt="Generic placeholder image" width="150" height="150">
-            <h2>Misión</h2>
-            <p>Ser una empresa innovadora y con gran sentido de responsabilidad social que busca el máximo beneficio de nuestros clientes a través de la calidad de nuestros productos y nuestros servicios y ofrecer productos y servicios que estén a la vanguardia de las últimas tendencias que el mercado demanda.</p>
+    <div class="container marketing">
+        <div class="row">
+            <div class="col-lg-4" id="j">
+                <img class="img-circle" src="https://g2consultores.com.mx/wp-content/uploads/2015/04/2010112419192700001105280000193577.jpg" alt="Generic placeholder image" width="150" height="150">
+                <h2>Misión</h2>
+                <p>Ser una empresa innovadora y con gran sentido de responsabilidad social que busca el máximo beneficio de nuestros clientes a través de la calidad de nuestros productos y nuestros servicios y ofrecer productos y servicios que estén a la vanguardia de las últimas tendencias que el mercado demanda.</p>
 
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4" id="j">
-            <img class="img-circle" src="https://image.freepik.com/vector-gratis/hombre-negocios-que-busca-carta-dinero-crecimiento-concepto-vision-ilustracion-vectorial-dibujos-animados_1212-1050.jpg" alt="Generic placeholder image" width="150" height="150">
-            <h2>Visión</h2>
-            <p>Ser un aliado estratégico para nuestros clientes basados en soluciones y servicios innovadores sobre la base de nuestra responsabilidad, calidad, productividad y compromiso.</p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4" id="j">
+                <img class="img-circle" src="https://image.freepik.com/vector-gratis/hombre-negocios-que-busca-carta-dinero-crecimiento-concepto-vision-ilustracion-vectorial-dibujos-animados_1212-1050.jpg" alt="Generic placeholder image" width="150" height="150">
+                <h2>Visión</h2>
+                <p>Ser un aliado estratégico para nuestros clientes basados en soluciones y servicios innovadores sobre la base de nuestra responsabilidad, calidad, productividad y compromiso.</p>
 
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4" id="j">
-            <img class="img-circle" src="https://10empresa.com/wp-content/uploads/2019/05/valores-de-una-empresa-1.gif" alt="Generic placeholder image" width="150" height="150">
-            <h2>Valores</h2>
-            <ul>
-                <li>Integridad: Honestos, justos y coherentes en todas nuestras acciones.</li>
-                <li>Responsabilidad: Asumir y cumplir trabajos.</li>
-                <li>Espíritu de Equipo: Inspirar el trabajo en equipo.</li>
-                <li>Respeto Mutuo: Cordialidad tanto entre el grupo.</li>
-            </ul>
-        </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4" id="j">
+                <img class="img-circle" src="https://10empresa.com/wp-content/uploads/2019/05/valores-de-una-empresa-1.gif" alt="Generic placeholder image" width="150" height="150">
+                <h2>Valores</h2>
+                <ul>
+                    <li>Integridad: Honestos, justos y coherentes en todas nuestras acciones.</li>
+                    <li>Responsabilidad: Asumir y cumplir trabajos.</li>
+                    <li>Espíritu de Equipo: Inspirar el trabajo en equipo.</li>
+                    <li>Respeto Mutuo: Cordialidad tanto entre el grupo.</li>
+                </ul>
+            </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
 
-    <!-- nueva parte -->
-    <!-- START THE FEATURETTES -->
+        <!-- nueva parte -->
+        <!-- START THE FEATURETTES -->
 
-    <hr class="featurette-divider">
+        <hr class="featurette-divider">
 
-    <div class="row featurette">
-        <div class="col-md-7">
-            <h2 class="featurette-heading">Quienes <span class="text-muted">Somos.</span></h2>
-            <p class="lead">Somos TECNO-SHOP, una empresa dedicada a la comercialización de productos electronicos, somos mas que una empresa, una familia, que dedicamos nuestro tiempo a encontrar el producto de su preferencia.</p>
+        <div class="row featurette">
+            <div class="col-md-7" id="centro">
+                <h2 class="featurette-heading">Quienes <span class="text-muted">Somos.</span></h2>
+                <p class="lead">Somos TECNO-SHOP, una empresa dedicada a la comercialización de productos electronicos, somos mas que una empresa, una familia, que dedicamos nuestro tiempo a encontrar el producto de su preferencia.</p>
+            </div>
+            <div class="col-md-5" id="k">
+                <img  src="https://www.estrategiaynegocios.net/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=m1EM7q9kezsU2ykITbnfP8$daE2N3K4ZzOUsqbU5sYtlpI5MGv1kT9gZkSPyzj1CWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&CONTENTTYPE=image/jpeg" alt="Generic placeholder image">
+            </div>
         </div>
-        <div class="col-md-5" id="k">
-            <img class="featurette-image img-responsive center-block" src="https://www.estrategiaynegocios.net/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=m1EM7q9kezsU2ykITbnfP8$daE2N3K4ZzOUsqbU5sYtlpI5MGv1kT9gZkSPyzj1CWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&CONTENTTYPE=image/jpeg" alt="Generic placeholder image">
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7 col-md-pull-5" id="k">
+                <img  src="https://i1.wp.com/globalmedia-it.com/wp-content/uploads/2017/11/1.jpg?fit=800%2C500&ssl=1" alt="Generic placeholder image">
+            </div>
+            <div class="col-md-5 col-md-push-7" id="centro">
+                <h2 class="featurette-heading">Que <span class="text-muted">Ofrecemos.</span></h2>
+                <p class="lead">Nosotros ofrecemos productos y servicios que estén a la vanguardia de las últimas tendencias que el mercado demanda.</p>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7" id="centro">
+                <h2 class="featurette-heading">El futuro de <span class="text-muted">TECNO-SHOP.</span></h2>
+                <p class="lead">Nuestra meta como empresa es llegar a expandirnos a nivel nacional, sabemos que tenems la confianza de todos nuestros clientes, que tarde o tempranos nos convertiremos en mas que comerciantes, llegar a ser proveedores de felicidad, quienes han logrado conectarnos con el futuro, con lo mejor que hay en el mercado electronico.</p>
+            </div>
+            <div class="col-md-5" id="k">
+                <img  src="https://image.freepik.com/foto-gratis/fila-gente-negocios-trabajando-computadoras-manos-empleados-escribiendo-teclados-portatiles_74855-8178.jpg" alt="Generic placeholder image">
+            </div>
         </div>
     </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-            <h2 class="featurette-heading">Que <span class="text-muted">Ofrecemos.</span></h2>
-            <p class="lead">Nosotros ofrecemos productos y servicios que estén a la vanguardia de las últimas tendencias que el mercado demanda.</p>
-        </div>
-        <div class="col-md-5 col-md-pull-7" id="k">
-            <img class="featurette-image img-responsive center-block" src="https://i1.wp.com/globalmedia-it.com/wp-content/uploads/2017/11/1.jpg?fit=800%2C500&ssl=1" alt="Generic placeholder image">
-        </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-        <div class="col-md-7">
-            <h2 class="featurette-heading">El futuro de <span class="text-muted">TECNO-SHOP.</span></h2>
-            <p class="lead">Nuestra meta como empresa es llegar a expandirnos a nivel nacional, sabemos que tenems la confianza de todos nuestros clientes, que tarde o tempranos nos convertiremos en mas que comerciantes, llegar a ser proveedores de felicidad, quienes han logrado conectarnos con el futuro, con lo mejor que hay en el mercado electronico.</p>
-        </div>
-        <div class="col-md-5" id="k">
-            <img class="featurette-image img-responsive center-block" src="https://image.freepik.com/foto-gratis/fila-gente-negocios-trabajando-computadoras-manos-empleados-escribiendo-teclados-portatiles_74855-8178.jpg" alt="Generic placeholder image">
-        </div>
-    </div>
-
 </div>
 @endsection
