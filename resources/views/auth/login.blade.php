66fc664f684e47a254ef02e9311fd46fa34eeaaf
JorgeG18 <jorge1giron18@gmail.com>
2021-04-15 19:44:11 -0600

    Arreglando conflictos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,8 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header" style="font-size: 20px; background-color:blue;:bold">{{ __('Login') }}</div>
-                <div class="card-header">{{ __('Inicias Sesión') }}</div>
+                <div class="card-header"  style="font-size: 20px; background-color:blue;:bold">{{ __('Inicias Sesión') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -56,7 +55,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Acceso') }}
+                                    {{ __('Iniciar Sesión') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
